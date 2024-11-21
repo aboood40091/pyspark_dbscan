fe829f606ef1a8ef7988c9650e2e12415bc667a0
AboodXD <aboood40091@gmail.com>
2024-11-22 00:21:25 +0400

    Upload main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from DistributedDbscan import ArgsParser, DebugHelper, IOHelper, DbscanSettings, PartitioningSettings, DistributedDbscan
+
+from pyspark.sql import SparkSession
+
+
+def main():
+    argsParser = ArgsParser()
+    argsParser.parse()
+    args = argsParser.args
+
+    builder: SparkSession.Builder = SparkSession.builder
+    spark = builder \
+        .master(args.masterUrl) \
+        .appName("DBSCAN") \
+        .config("spark.files", args.pyFiles) \
+        .getOrCreate()
+
+    if args.debugOutputPath:
+        spark.conf.set(DebugHelper.DebugOutputPath, args.debugOutputPath)
+
+    data = IOHelper.readDataset(spark.sparkContext, args.inputPath)
+    settings = DbscanSettings() \
+        .withEpsilon(args.eps) \
+        .withNumberOfPoints(args.minPts) \
+        .withTreatBorderPointsAsNoise(args.borderPointsAsNoise) \
+        .withDistanceMeasure(args.distanceMeasure)
+
+    partitioningSettings = PartitioningSettings(numberOfPointsInBox=args.numberOfPoints)
+    clusteringResult = DistributedDbscan.train(data, settings, partitioningSettings)
+    IOHelper.saveClusteringResult(clusteringResult, args.outputPath)
+
+
+if __name__ == '__main__':
+    main()
